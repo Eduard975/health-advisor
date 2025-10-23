1616789c8fb5bd95a3be4fdd7ab837ea863192ad
SilviuDairrow <izotasilviu@gmail.com>
2025-10-23 15:36:03 +0300

    Edit backend + config firebase

--- orechestrator-service/utils/helpers.go
+++ orechestrator-service/utils/helpers.go
@@ -3,10 +3,17 @@ package utils
 import (
 	"crypto/rand"
 	"fmt"
+	"log"
+	"time"
 )
 
 func GenerateID() string {
 	b := make([]byte, 16)
-	rand.Read(b)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Printf("Error generating ID: %v", err)
+		// Fallback to timestamp-based ID
+		return fmt.Sprintf("%d", time.Now().UnixNano())
+	}
 	return fmt.Sprintf("%x", b)
 }
